aa15e3faaf530d527f3df771b02321173d0c2459
PilzAdam <pilzadam@minetest.net>
2013-05-22 22:55:57 +0200

    Dont show selectionbox of dropped items all the time

--- mods/item_drop/init.lua
+++ mods/item_drop/init.lua
@@ -34,8 +34,10 @@ minetest.register_globalstep(function(dtime)
 							vec.z = vec.z*3
 							object:setvelocity(vec)
 							object:get_luaentity().physical_state = false
+							object:get_luaentity().flying = false
 							object:get_luaentity().object:set_properties({
-								physical = false
+								physical = false,
+								collisionbox = {0,0,0, 0,0,0},
 							})
 							
 							minetest.after(1, function(args)
@@ -57,7 +59,8 @@ minetest.register_globalstep(function(dtime)
 									object:setvelocity({x=0,y=0,z=0})
 									bject:get_luaentity().physical_state = true
 									object:get_luaentity().object:set_properties({
-										physical = true
+										physical = true,
+										collisionbox = {-0.17,-0.17,-0.17, 0.17,0.17,0.17},
 									})
 								end
 							end, {player, object})
@@ -111,6 +114,119 @@ function minetest.handle_node_drops(pos, drops, digger)
 	end
 end
 
-if minetest.setting_get("log_mods") then
-	minetest.log("action", "item_drop loaded")
-end
+minetest.register_entity(":__builtin:item", {
+	initial_properties = {
+		hp_max = 1,
+		physical = true,
+		collisionbox = {-0.17,-0.17,-0.17, 0.17,0.17,0.17},
+		visual = "sprite",
+		visual_size = {x=0.5, y=0.5},
+		textures = {""},
+		spritediv = {x=1, y=1},
+		initial_sprite_basepos = {x=0, y=0},
+		is_visible = false,
+	},
+	
+	itemstring = '',
+	physical_state = true,
+	flying = false,
+
+	set_item = function(self, itemstring)
+		self.itemstring = itemstring
+		local stack = ItemStack(itemstring)
+		local itemtable = stack:to_table()
+		local itemname = nil
+		if itemtable then
+			itemname = stack:to_table().name
+		end
+		local item_texture = nil
+		local item_type = ""
+		if minetest.registered_items[itemname] then
+			item_texture = minetest.registered_items[itemname].inventory_image
+			item_type = minetest.registered_items[itemname].type
+		end
+		prop = {
+			is_visible = true,
+			visual = "sprite",
+			textures = {"unknown_item.png"}
+		}
+		if item_texture and item_texture ~= "" then
+			prop.visual = "sprite"
+			prop.textures = {item_texture}
+			prop.visual_size = {x=0.50, y=0.50}
+		else
+			prop.visual = "wielditem"
+			prop.textures = {itemname}
+			prop.visual_size = {x=0.20, y=0.20}
+			prop.automatic_rotate = math.pi * 0.25
+		end
+		self.object:set_properties(prop)
+	end,
+
+	get_staticdata = function(self)
+		--return self.itemstring
+		return minetest.serialize({
+			itemstring = self.itemstring,
+			always_collect = self.always_collect,
+		})
+	end,
+
+	on_activate = function(self, staticdata)
+		if string.sub(staticdata, 1, string.len("return")) == "return" then
+			local data = minetest.deserialize(staticdata)
+			if data and type(data) == "table" then
+				self.itemstring = data.itemstring
+				self.always_collect = data.always_collect
+			end
+		else
+			self.itemstring = staticdata
+		end
+		self.object:set_armor_groups({immortal=1})
+		self.object:setvelocity({x=0, y=2, z=0})
+		self.object:setacceleration({x=0, y=-10, z=0})
+		self:set_item(self.itemstring)
+	end,
+
+	on_step = function(self, dtime)
+		if self.flying then
+			return
+		end
+		local p = self.object:getpos()
+		p.y = p.y - 0.3
+		local nn = minetest.env:get_node(p).name
+		-- If node is not registered or node is walkably solid and resting on nodebox
+		local v = self.object:getvelocity()
+		if not minetest.registered_nodes[nn] or minetest.registered_nodes[nn].walkable and v.y == 0 then
+			if self.physical_state then
+				self.object:setvelocity({x=0,y=0,z=0})
+				self.object:setacceleration({x=0, y=0, z=0})
+				self.physical_state = false
+				self.object:set_properties({
+					physical = false,
+					collisionbox = {0,0,0, 0,0,0},
+				})
+			end
+		else
+			if not self.physical_state then
+				self.object:setvelocity({x=0,y=0,z=0})
+				self.object:setacceleration({x=0, y=-10, z=0})
+				self.physical_state = true
+				self.object:set_properties({
+					physical = true,
+					collisionbox = {-0.17,-0.17,-0.17, 0.17,0.17,0.17},
+				})
+			end
+		end
+	end,
+
+	on_punch = function(self, hitter)
+		if self.itemstring ~= '' then
+			local left = hitter:get_inventory():add_item("main", self.itemstring)
+			if not left:is_empty() then
+				self.itemstring = left:to_string()
+				return
+			end
+		end
+		self.object:remove()
+	end,
+})
