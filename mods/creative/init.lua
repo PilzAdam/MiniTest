b89550cf98d28fd9f643a588fc5c986a53e2a6aa
PilzAdam <pilzadam@minetest.net>
2013-05-24 13:42:31 +0200

    Make creative digging faster

--- mods/creative/init.lua
+++ mods/creative/init.lua
@@ -134,11 +134,11 @@ if minetest.setting_getbool("creative_mode") then
 			full_punch_interval = 0.5,
 			max_drop_level = 3,
 			groupcaps = {
-				crumbly = {times={[1]=0.5, [2]=0.5, [3]=0.5}, uses=0, maxlevel=3},
-				cracky = {times={[1]=0.5, [2]=0.5, [3]=0.5}, uses=0, maxlevel=3},
-				snappy = {times={[1]=0.5, [2]=0.5, [3]=0.5}, uses=0, maxlevel=3},
-				choppy = {times={[1]=0.5, [2]=0.5, [3]=0.5}, uses=0, maxlevel=3},
-				oddly_breakable_by_hand = {times={[1]=0.5, [2]=0.5, [3]=0.5}, uses=0, maxlevel=3},
+				crumbly = {times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
+				cracky = {times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
+				snappy = {times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
+				choppy = {times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
+				oddly_breakable_by_hand = {times={[1]=0, [2]=0, [3]=0}, uses=0, maxlevel=3},
 			},
 			damage_groups = {fleshy = 10},
 		}
