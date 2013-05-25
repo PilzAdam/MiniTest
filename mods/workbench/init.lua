e4c893d406a90fe59c76267d6c5c16a28791f809
PilzAdam <pilzadam@minetest.net>
2013-05-25 22:11:52 +0200

    Fix all problems with workbench

--- mods/workbench/init.lua
+++ mods/workbench/init.lua
@@ -1,72 +1,16 @@
-
-local function update_workbench(pos)
-	local meta = minetest.env:get_meta(pos)
-	local inv = meta:get_inventory()
-	local result = minetest.get_craft_result({
-		type = "normal",
-		width = 3,
-		items = inv:get_list("craft")
-	})
-	inv:set_stack("result", 1, result.item)
-end
-
 minetest.register_node("workbench:workbench", {
 	description = "Workbench",
 	tiles = {"workbench_top.png", "workbench_side.png"},
 	stack_max = 64,
 	groups = {choppy=2,oddly_breakable_by_hand=2,flammable=3},
 	sounds = default.node_sound_wood_defaults(),
-	on_construct = function(pos)
-		local meta = minetest.env:get_meta(pos)
-		local inv = meta:get_inventory()
-		inv:set_size("craft", 9)
-		inv:set_size("result", 1)
-		meta:set_string("formspec", 
+	on_rightclick = function(pos, node, clicker, itemstack)
+		minetest.show_formspec(clicker:get_player_name(), "workbench:workbench",
 			"size[9,7.5;]"..
 			"list[current_player;main;0,3.5;9,4;]"..
-			"list[current_name;craft;4,0;3,3;]"..
-			"list[current_name;result;7.5,1;1,1;]"
+			"list[current_player;craft;4,0;3,3;]"..
+			"list[current_player;craftpreview;7.5,1;1,1;]"
 		)
-	end,
-	allow_metadata_inventory_move = function(pos, from_list, from_index,to_list, to_index, count, player)
-		if to_list == "result" then
-			return 0
-		end
-		return count
-	end,
-	allow_metadata_inventory_put = function(pos, listname, index, stack, player)
-		if listname == "result" then
-			return 0
-		end
-		return stack:get_count()
-	end,
-	allow_metadata_inventory_take = function(pos, listname, index, stack, player)
-		return stack:get_count()
-	end,
-	on_metadata_inventory_put = update_workbench,
-	on_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
-		local inv = minetest.env:get_meta(pos):get_inventory()
-		if from_list == "result" then
-			for i,stack in ipairs(inv:get_list("craft")) do
-				stack:take_item()
-				inv:set_stack("craft", i, stack)
-			end
-		end
-		update_workbench(pos)
-	end,
-	on_metadata_inventory_take = function(pos, listname, index, stack, player)
-		local inv = minetest.env:get_meta(pos):get_inventory()
-		if listname == "result" then
-			for i,stack in ipairs(inv:get_list("craft")) do
-				stack:take_item()
-				inv:set_stack("craft", i, stack)
-			end
-		end
-		update_workbench(pos)
-	end,
-	can_dig = function(pos)
-		local inv = minetest.env:get_meta(pos):get_inventory()
-		return inv:is_empty("craft") and inv:is_empty("result")
 	end,
 })
 
@@ -80,13 +24,17 @@ minetest.register_craft({
 
 minetest.register_on_joinplayer(function(player)
 	if not minetest.setting_getbool("creative_mode") then
-		player:get_inventory():set_width("craft", 2)
-		player:get_inventory():set_size("craft", 4)
+		player:get_inventory():set_width("craft", 3)
+		player:get_inventory():set_size("craft", 9)
 		player:get_inventory():set_size("main", 9*4)
 		player:set_inventory_formspec(
 			"size[9,7.5;]"..
 			"list[current_player;main;0,3.5;9,4;]"..
-			"list[current_player;craft;5,0.5;2,2;]"..
+			
+			"list[current_player;craft;5,0.5;2,1;1]"..
+			
+			"list[current_player;craft;5,1.5;2,1;4]"..
+			
 			"list[current_player;craftpreview;7.5,1;1,1;]"
 		)
 	end
