dacb7577754de94c2924756dd9eae9d1816107bf
PilzAdam <pilzadam@minetest.net>
2013-05-26 15:30:44 +0200

    Fix hand digging wood faster than wooden/stone axe

--- mods/default/tools.lua
+++ mods/default/tools.lua
@@ -10,7 +10,7 @@ minetest.register_item(":", {
 		groupcaps = {
 			crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
 			snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
-			oddly_breakable_by_hand = {times={[1]=7.00,[2]=4.00,[3]=1.40}, uses=0, maxlevel=3}
+			oddly_breakable_by_hand = {times={[1]=7.00,[2]=4.00,[3]=1.40}, uses=0}
 		},
 		full_punch_interval = 1,
 		damage_groups = {fleshy=1},
